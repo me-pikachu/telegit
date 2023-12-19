fb45af566f62bdfd54a8ba63e68f861aeec4ba1b
Tanmay Singhal <64832633+singhaltanmay@users.noreply.github.com>
2023-12-19 17:20:30 +0530

    telegram push pull

--- src/telefuncs.py
+++ src/telefuncs.py
@@ -1,7 +1,11 @@
 from telegram.ext.updater import Updater
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
 import telegram
+import requests
+import json
+
 TOKEN = "6713242583:AAFEEy1jEhOyax8440-yMAF7owu5ClAXLVI"
 
 updater = Updater(TOKEN, use_context = True)
@@ -11,13 +15,28 @@ def start(update: Update, context: CallbackContext):
     update.message.reply_text("connection established!!")
 
     
-def totele(path, bot=bot):
+def totele(path, bot=bot, TOKEN=TOKEN):
     ids = open("E:/telegit/chatids.ids", 'r')
     l=ids.readlines()
     ids.close()
     chat_id = int(l[0])
-    document = open(path, 'rb')
-    bot.send_document(chat_id, document)
+    document = open(path, "rb")
+    url = f"https://api.telegram.org/bot{TOKEN}/sendDocument"
+    response = requests.post(url, data={'chat_id': chat_id}, files={'document': document})
+    content = response.content.decode("utf8")
+    js = json.loads(content)
+    return js['result']['document']['file_id']
+
+    
+
+def fromtele(id: str, topath: str, filename: str, bot=bot):
+    file = bot.getFile(id)
+    if (topath!=None):
+        file.download(topath+filename)
+    else:
+        file.download(filename)
+    print("Downloaded!!")
+
 
 def extractid(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
@@ -26,9 +45,12 @@ def extractid(update: Update, context: CallbackContext):
     ids.close()
     update.message.reply_text("extraction successfull!!")
 
-#totele("E:/telegit/chatids.ids")
+# file_id = totele("E:/telegit/chatids.ids")
+# fromtele(file_id, "E:/", "chatids.txt")
+
 #updater.dispatcher.add_handler(CommandHandler('start', start))
-#updater.dispatcher.add_handler(CommandHandler('extractme', extractid))
+#updater.dispatcher.
+# add_handler(CommandHandler('extractme', extractid))
 #updater.start_polling()
 #updater.idle()
 
