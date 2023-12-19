72829e43b3d2365a06767b9d48491af85ea55512
Tanmay Singhal <64832633+singhaltanmay@users.noreply.github.com>
2023-12-19 14:01:00 +0530

    initial

--- src/basicfuncs.py
+++ src/basicfuncs.py
@@ -30,4 +30,4 @@ def togit(path: str, folder, filename: str, gitoken: str, repo: str, desc: str):
             repo.create_file(f'{filename}', desc, data, branch='main')
     print(f'pushed {filename} successfully!!')
 
-togit("E:/telegit/basicfuncs.py", 'src', 'basicfuncs.py', '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', 'me-pikachu/telegit', 'updated todrive')
+togit("E:/telegit/basicfuncs.py", 'src', 'basicfuncs.py', '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', 'me-pikachu/telegit', 'initial')
